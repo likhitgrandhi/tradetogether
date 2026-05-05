63e19d962fcd361186b4da2d1a106ed0cd934216
Likhit Grandhi <likhit@hawcx.com>
2026-05-05 18:48:24 +0530

    Initial Commit

---
+++ tradetogether/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  tradetogether
+//
+//  Created by Likhit Grandhi on 05/05/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ tradetogetherTests/tradetogetherTests.swift
@@ -0,0 +1,17 @@
+//
+//  tradetogetherTests.swift
+//  tradetogetherTests
+//
+//  Created by Likhit Grandhi on 05/05/26.
+//
+
+import Testing
+@testable import tradetogether
+
+struct tradetogetherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
